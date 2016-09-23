335e5045853154f02c8b4fa64c0df7cd5b737a41
naresh242 <naresh.srikakolapu@gmail.com>
2016-09-23 22:07:57 +0530

    how to apply knn for images
    preprocessing images,tuning parameters,training model and predict the test data set code. it can be directly used in real time applications.
    
    but neural networks will give better performance than this model. check in NN section for the code

---
+++ imageknn.py
@@ -0,0 +1,58 @@
+
+from __future__ import print_function
+from sklearn.cross_validation import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import classification_report
+from sklearn import datasets
+from skimage import exposure
+import numpy as np
+import imutils
+import cv2
+ 
+
+mnist = datasets.load_digits()
+ 
+# take the MNIST data and construct the training and testing split, using 75% of the
+# data for training and 25% for testing
+(trainData, testData, trainLabels, testLabels) = train_test_split(np.array(mnist.data),
+	mnist.target, test_size=0.25, random_state=42)
+ 
+# now, let's take 10% of the training data and use that for validation
+(trainData, valData, trainLabels, valLabels) = train_test_split(trainData, trainLabels,
+	test_size=0.1, random_state=84)
+ 
+# show the sizes of each data split
+print("training data points: {}".format(len(trainLabels)))
+print("validation data points: {}".format(len(valLabels)))
+print("testing data points: {}".format(len(testLabels)))
+
+# initialize the values of k for our k-Nearest Neighbor classifier along with the
+# list of accuracies for each value of k
+kVals = range(1, 30, 2)
+accuracies = []
+ 
+# loop over various values of `k` for the k-Nearest Neighbor classifier
+for k in xrange(1, 30, 2):
+	# train the k-Nearest Neighbor classifier with the current value of `k`
+	model = KNeighborsClassifier(n_neighbors=k)
+	model.fit(trainData, trainLabels)
+ 
+	# evaluate the model and update the accuracies list
+	score = model.score(valData, valLabels)
+	print("k=%d, accuracy=%.2f%%" % (k, score * 100))
+	accuracies.append(score)
+ 
+# find the value of k that has the largest accuracy
+i = np.argmax(accuracies)
+print("k=%d achieved highest accuracy of %.2f%% on validation data" % (kVals[i],
+	accuracies[i] * 100))
+# re-train our classifier using the best k value and predict the labels of the
+# test data
+model = KNeighborsClassifier(n_neighbors=kVals[i])
+model.fit(trainData, trainLabels)
+predictions = model.predict(testData)
+ 
+# show a final classification report demonstrating the accuracy of the classifier
+# for each of the digits
+print("EVALUATION ON TESTING DATA")
+print(classification_report(testLabels, predictions))	
